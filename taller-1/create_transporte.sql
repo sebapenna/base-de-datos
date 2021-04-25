0a65ebbb8eafe47ba8db74361108275f66a588e1
spenna <sebapenna19@gmail.com>
2021-04-24 21:07:51 -0300

    Taller 2

--- taller-1/create_transporte.sql
+++ taller-1/create_transporte.sql
@@ -1,10 +1,11 @@
 -- script: 'creacion_transporte'
 -- resumen: crea dos tablas sobre las paradas y los colectivos por cada parada
 -- fecha creacion: 17-04-21
+-- ultima modificacion: 24-04-21
 -- autor: Sebastian Penna
 
-DROP TABLE IF EXISTS public.paradas;
 DROP TABLE IF EXISTS public.colectivos_por_parada;
+DROP TABLE IF EXISTS public.paradas;
 
 CREATE TABLE public.paradas (
 	cod_parada	INTEGER	NOT NULL,
@@ -14,10 +15,21 @@ CREATE TABLE public.paradas (
 	calle		VARCHAR(50),
 	altura		INTEGER,
 	entre1		VARCHAR(50),
-	entre2		VARCHAR(50)
+	entre2		VARCHAR(50),
+	CONSTRAINT	PK_paradas PRIMARY KEY(cod_parada)
 );
 
 CREATE TABLE public.colectivos_por_parada(
 	cod_parada		INTEGER	NOT NULL,
-	num_colectivo	INTEGER	NOT NULL
+	num_colectivo	INTEGER	NOT NULL,
+	CONSTRAINT FK_colectivos_por_parada FOREIGN KEY(cod_parada) REFERENCES public.paradas(cod_parada) ON DELETE NO ACTION,
+	CONSTRAINT	PK_colectivos_por_parada PRIMARY KEY(cod_parada, num_colectivo)
 );
+
+ALTER TABLE public.colectivos_por_parada
+			DROP CONSTRAINT FK_colectivos_por_parada,
+			ADD CONSTRAINT FK_colectivos_por_parada 
+				FOREIGN KEY(cod_parada) 
+				REFERENCES public.paradas(cod_parada) 
+				ON DELETE NO ACTION
+				ON UPDATE CASCADE;
--- taller-1/insert_transporte.sql
+++ taller-1/insert_transporte.sql
@@ -1,8 +1,13 @@
 -- script: 'insert_transporte'
 -- resumen: carga un dato en la tabla paradas
 -- fecha creacion: 17-04-21
+-- ultima modificacion: 24-04-21
 -- autor: Sebastian Penna
 
 INSERT INTO public.paradas(
 	cod_parada, longitud, latitud, tipo_parada, calle, altura, entre1, entre2)
 	VALUES (1, 12.10, 10.12, 'comun', 'Belgrano', 1233, 'Libertad', 'Paseo Colon');
+
+
+INSERT INTO public.colectivos_por_parada(cod_parada, num_colectivo)
+	VALUES (1, 28);
