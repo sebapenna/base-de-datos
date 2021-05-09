0600e5635d2d312135c27b97e16ce5a95c4b2448
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2021-05-09 11:15:49 -0300

    Update taller4-18.sql

--- taller-4/taller4-18.sql
+++ taller-4/taller4-18.sql
@@ -9,12 +9,3 @@ EXCEPT
 SELECT n.padron, a.nombre
 FROM notas n
 INNER JOIN alumnos a ON(codigo = 75 AND numero = 15 AND nota < 4 AND a.padron = n.padron);
-
--- Respuesta clase
-SELECT a.padron, a.nombre 
-FROM alumnos a INNER JOIN notas n USING(padron)
-WHERE n.codigo = 71 AND n.numero = 14 AND n.nota >= 4
-EXCEPT
-SELECT a.padron, a.nombre 
-FROM alumnos a INNER JOIN notas n USING(padron)
-WHERE n.codigo = 71 AND n.numero = 15 AND n.nota >= 4
