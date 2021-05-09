f0dee0282fac7c47e62f1c45865d4fb343f4cbf4
Sebastian Penna <31221128+sebapenna@users.noreply.github.com>
2021-05-09 11:15:39 -0300

    Update taller4-17.sql

--- taller-4/taller4-17.sql
+++ taller-4/taller4-17.sql
@@ -7,10 +7,3 @@ FROM notas n
 WHERE n.codigo = 75 AND n.numero = 15
 GROUP BY padron
 HAVING COUNT(nota) > 1;
-
--- Solucion clase
-SELECT DISTINCT n1.padron
-FROM notas n1, notas n2
-WHERE n1.padron = n2.padron AND n1.codigo = n2.codigo
-AND n1.numero = n2.numero AND n1.fecha > n2.fecha
-AND n1.codigo = 75 AND n1.numero = 15;
